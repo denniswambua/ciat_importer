8cb9ab101993878a90cdc84b330aa0f527363f7c
denniswambua <dennis.wambua@gmail.com>
2019-06-13 14:11:08 +0300

    read xls

--- importer.py
+++ importer.py
@@ -1,3 +1,5 @@
+import xlrd
+
 from connection import Connection
 
 
@@ -31,11 +33,52 @@ class CIATDataImporter:
 
             print("Table created")
 
+    def insert_record(self, values):
+        sql = 'insert into socio_monitorig_main ({}) values ("{}");'
+
+        columns = ','.join(self.header)
+        value = '","'.join(values) 
+        insert_sql = sql.format(columns, value)
+        print(insert_sql)
+        with self.connection.cursor() as cursor:
+            # execute SQL query using execute() method.
+            cursor.execute(insert_sql)
+            connection.commit()
+
+            print("Record Inserted")
+
+    def set_headers(self, sheet):
+        self.header = []
+        tmp_header = self.get_values(sheet, 0)
+
+        for hh in tmp_header:
+            hh = hh.replace("/", "_")
+            self.header.append(hh)
+
+    def get_values(self, sheet, index):
+        values = []
+        for i in range(sheet.ncols): 
+            values.append(sheet.cell_value(0, i))
+
+        return values
+
+    def read_xls(self, loc):
+        wb = xlrd.open_workbook(loc) 
+        sheet = wb.sheet_by_index(0)
+        
+        self.set_headers(sheet)
+
+        values = self.get_values(sheet, 1)
+
+        self.insert_record(values)
+        
+
 
 if __name__ == "__main__":
     connection = Connection.get_instance()
     try:
         importer = CIATDataImporter(connection)
-        importer.loader()
+        #importer.loader()
+        importer.read_xls("db/test.xlsx")
     finally:
         connection.close()
